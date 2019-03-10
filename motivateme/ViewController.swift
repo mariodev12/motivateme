759c6f3d49308c3f447924e3948c8e64563a3e79
Mario <contacto@mariodiezgarcia.com>
2019-03-10 22:56:21 +0100

    Initial Commit

---
+++ motivateme/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  motivateme
+//
+//  Created by Mario on 10/03/2019.
+//  Copyright © 2019 Mario. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
